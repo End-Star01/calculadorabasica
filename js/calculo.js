009050dc5505844a43b6a2deea6618e33e59e144
End-Star01 <anavanessa162@gmail.com>
2023-04-15 23:54:56 -0300

    a

--- js/calculo.js
+++ js/calculo.js
@@ -1,24 +1,34 @@
 // seleção dos eventos
-const previousOperationText = document.querySelector("#aprevious-operatio");
-const currentOperationText = document.querySelector("#acurrent-operation");
-const buttons = document.querySelectorAll("#buttons-container button");
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#button-container button");
 
 
 class Calculator {
+    constructor(previousOperationText, currentOperationText){
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ""
+    }
 
+    addDigit(digit) {
+        console.log(digit);
+    }
 }
 
+const calc = new Calculator(previousOperationText,currentOperationText)
+
 // eventos
-buttons.forEach(btn) => {
+buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        const value = e.target.innerText;
+        const value = e.target.innerTEXT;
 
         console.log(value);
 
         if(+value >= 0 || value === "."){
-            console.log(valor);
+            calc.addDigit(value);
         } else {
             console.log('op:' + value);
         }
     });
-}
+})
