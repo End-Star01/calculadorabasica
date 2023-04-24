3613de76a080b5271ee7395e4c41ef2e4d07e2dd
End-Star01 <anavanessa162@gmail.com>
2023-04-24 19:39:30 -0300

    a

--- js/calculo.js
+++ js/calculo.js
@@ -12,10 +12,56 @@ class Calculator {
     }
 
     addDigit(digit) {
-        console.log(digit);
+        if(digit === "." && this.currentOperationText.innerHTML.includes(".")){
+            return;
+        }
+
         this.currentOperation = digit
+        this.updateScreen()
+    }
+
+processOperation(operation){
+    let operationValue
+    const previous = +this.previousOperationText.innerHTML;
+    const current = +this.currentOperationText.innerHTML;
+
+    switch (operation) {
+        case "+":
+            operationValue = previous + current
+            this.updateScreen(operationValue, operation, current, previous)
+            break;
+        default:
+            return;
     }
 }
+
+    updateScreen(
+        operationValue = null,
+        operation = null,
+        current = null,
+        previous = null
+         )
+         {
+            if(operationValue === null){
+                this.currentOperationText.innerHTML += this.currentOperation;
+            } else {
+                if(previous === 0){
+                    operationValue = current
+                }
+
+                this.previousOperationText.innerHTML = `${operationValue} ${operation}`
+                this.currentOperationText.innerHTML = "";
+            }
+
+
+
+            console.log(operationValue,operation,current,previous);
+
+            this.currentOperationText.innerHTML += this.currentOperation;
+            
+    }
+}
+
 
 const calc = new Calculator(previousOperationText,currentOperationText)
 
@@ -23,6 +69,13 @@ const calc = new Calculator(previousOperationText,currentOperationText)
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerHTML;
-        console.log(value);
+        
+
+        if(+value >= 0 || value === "."){
+            calc.addDigit(value)
+        } else {
+            calc.processOperation(value);
+        }
+
     })
 })
