0a85546a1037fbe87df11ee93f7369cee5e2d028
End-Star01 <anavanessa162@gmail.com>
2023-05-08 13:12:46 -0300

    a

--- calculadora.teste/cypress/e2e/calculadora.cy.js
+++ calculadora.teste/cypress/e2e/calculadora.cy.js
@@ -6,7 +6,10 @@ describe("testando função soma", () => {
     })
 
     it("soma de 3 + 5", ()=>{
-        cy.get('#numero1')
+        cy.get('#inum1').type("3")
+        cy.get('#inum2').type("5")
+        cy.get('#ifunct').click();
+        cy.get('#resultado').should("have.text", "8")
 
 
     })
